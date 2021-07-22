0de151b52d627ae1398e20ec6b4901f1b343774a
Ian Solano-Kamaiko <2371418+iansolano@users.noreply.github.com>
2021-07-22 15:11:45 -0400

    update run_script file and add notebook batch script

--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -13,32 +13,12 @@ echo "****** Convert notebook file to script ******"
 jupyter nbconvert --to script $HOME/scripts/$2.ipynb
 
 echo "****** Starting postgres slurm job ******"
+cp $HOME/scripts/postgresql.sbatch $SCRATCH/postgresql.sbatch
 cd $SCRATCH
 mkdir -p logs
-
 sbatch --mail-user=$1 --time=$3 --output=$SCRATCH/logs/postgres-%J.log postgresql.sbatch
 
 echo "****** Starting jupyter notebook slurm job ******"
 cd -
 mkdir -p logs
-
-sbatch <<EOT
-#!/bin/bash
-
-#SBATCH --job-name=jupyter
-#SBATCH --mail-user=$1
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=64GB
-#SBATCH --time=$3
-#SBATCH --output=$HOME/scripts/logs/jupyter-notebook-%J.log
-
-module purge
-module load python/intel/3.8.6
-
-pip install --no-index --upgrade pip
-pip install --no-index -r requirements.txt
-python $HOME/scripts/$2.py
-EOT
+sbatch --mail-user=$1 --time=$3 --output=$HOME/scripts/logs/jupyter-notebook-%J.log notebook.sbatch
